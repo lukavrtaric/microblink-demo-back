cee6ae967939e9ad43349081b1d2fa5a9a3e259a
Neil Down <fullstackoneday@gmail.com>
2019-09-07 11:05:26 +0200

    heroku config for mongodb added

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 const config = {
-    mongodb: 'mongodb://mongodb:27017/microblink'
+    //mongodb: 'mongodb://mongodb:27017/microblink'
+    mongodb: 'mongodb://heroku_x9b7j7g4:9foikegflfcjhlkhs67mgf1a4g@ds219078.mlab.com:19078/heroku_x9b7j7g4'
 };
 
 module.exports = config;
